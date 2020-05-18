1b4cb05d51e1395b160b18909ce68c700c020dfb
Leonhard Seidel <leonhardseidel@gmx.de>
2020-05-18 14:53:00 +0200

    fixed the square example and found the "cause" for the couloumn drift

--- example/drawing_example/drawing_example.ino
+++ example/drawing_example/drawing_example.ino
@@ -1,12 +1,21 @@
 #include <Panel.h>
 
 Panel panel(32, 64, true);
-
+int i = 0;
 void setup() {
-    
+
 }
 
 void loop() {
-    panel.selectLine(millis()/1000);
-    panel.sendWholeRow(1, 1, 1, 1, 1, 1);
+	if (i / 1000 < 16) {
+		panel.selectLine(i / 1000);
+		panel.sendWholeRow(1, 1, 1, 1, 1, 1);
+		if (i % 1000 == 0) {
+			Serial.println(i / 1000);
+		}
+	}
+	i++;
+	if (i > 16000) {
+		i = 0;
+	}
 }
